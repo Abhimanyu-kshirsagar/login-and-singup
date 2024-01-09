7e891274fb469244d98048b09fe811a0dbab3509
Abhimanyu-kshirsagar <145312922+Abhimanyu-kshirsagar@users.noreply.github.com>
2024-01-09 13:45:02 +0530

    Add files via upload

---
+++ project1 nextus/script.js
@@ -0,0 +1,18 @@
+function toggleForm(formType) {
+    const loginForm = document.getElementById('login-form');
+    const signupForm = document.getElementById('signup-form');
+    const loginToggle = document.getElementById('login-toggle');
+    const signupToggle = document.getElementById('signup-toggle');
+
+    if (formType === 'login') {
+        loginForm.style.display = 'flex';
+        signupForm.style.display = 'none';
+        loginToggle.classList.add('active');
+        signupToggle.classList.remove('active');
+    } else {
+        loginForm.style.display = 'none';
+        signupForm.style.display = 'flex';
+        loginToggle.classList.remove('active');
+        signupToggle.classList.add('active');
+    }
+}
